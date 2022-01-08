04a22cee0d6b85754f4e61b1af0f9531e5dc0273
mespinozar <rieraes@gmail.com>
2022-01-08 17:32:27 -0500

    first dashboard

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+
+sidebarBtn.onclick = function() {
+    sidebar.classList.toggle("active");
+}
